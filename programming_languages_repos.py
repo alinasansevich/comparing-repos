0c6240b6c0864a14d102f282592b6d4fdc3df7dd
Alina Sansevich <alinasansevich@gmail.com>
2020-10-07 15:27:01 -0500

    Reorganizing.

--- programming_languages_repos.py
+++ programming_languages_repos.py
@@ -32,7 +32,7 @@ def get_contributors_num(contributors_dict):
     """
     (dict) -> dict
 
-    Returns a new dict, with the repos names as keys
+    Returns a new dict, with the repos' names as keys
     and the number of contributors as values.
     """
     num_contributors = {}
@@ -40,7 +40,6 @@ def get_contributors_num(contributors_dict):
     for k, v in contributors_dict.items():
         url = v + '?page=1&per_page=1000'
         r = requests.get(url)
-        
         response_dict = r.json()
         num_contributors[k] = len(response_dict)
     
@@ -48,10 +47,7 @@ def get_contributors_num(contributors_dict):
 
 
 # Make an API call for each language and store the responses.
-repos_names = {}
-repos_info = {}
-num_contributors = {}
-
+raw_data = {} # raw_data[language] = [{74 k,v} * 30 repos]
 for language in languages:
     url = 'https://api.github.com/search/repositories?q=language:{}&sort=stars'
     url = url.format(language)
@@ -59,30 +55,60 @@ for language in languages:
     response_dict = r.json()
     # Create a list to hold the info from the 30 most starred repos. 
     repo_dicts = response_dict['items']
+    raw_data[language] = repo_dicts
     
-    # Extract info for the graphs.
+# Extract info for the most starred projects graph.
+repos_names = {} # k:language, v:list of repos_names
+repos_info = {}  # k:'value', v: stargazers_count; k:'xlink', v:'html-url'
+for language in languages:
     names, plot_dicts = [], []
-    contributors_urls = {}
-    for repo_dict in repo_dicts:
-        
+    for repo_dict in raw_data[language]:
         names.append(repo_dict['name'])
-        
         plot_dict = {
             'value': repo_dict['stargazers_count'],
             'label': repo_dict['description'] or '',
             'xlink': repo_dict['html_url'],
             }
         plot_dicts.append(plot_dict)
-        
-        contributors_urls[repo_dict['name']] = repo_dict['contributors_url']
-        
     repos_names[language] = names
     repos_info[language] = plot_dicts
-    # Make an API call to get the number of contributors
+
+# Make an API call to get the number of contributors for each repo
+num_contributors = {}  # k-language, v-{repo-name: num_contributors}
+for language in languages:
+    contributors_urls = {}
+    for repo_dict in raw_data[language]:
+        contributors_urls[repo_dict['name']] = repo_dict['contributors_url']
     num_contributors[language] = get_contributors_num(contributors_urls)
 
+top_5_repos = {}
+for language, repos_names in repos_names.items():
+    top_5 = repos_names[:5]
+    top_5_repos[language] = top_5
+
+top_5_xlinks = {}
+for language, repos_info_list in repos_info.items():
+    xlinks = []
+    for i in range(5):
+        xlink = repos_info_list[i]['xlink']
+        xlinks.append(xlink)
+    top_5_xlinks[language] = xlinks
+
+top_5_num_contrib = {}
+for language, repos_contrib in num_contributors.items():
+    top_5_contrib = []
+    for name in top_5_repos[language]:
+        num = repos_contrib[name]
+        top_5_contrib.append(num)
+    top_5_num_contrib[language] = top_5_contrib
 
 
+######## ESTOY ACA
+# for the top 5:
+# 	name of the repo
+# 	html-url of the repo
+# 	number of contributors
+    
 
 # Make visualization.
 colors = ['#333366',
